56dd1f6ad6bc7b227db66969db89c1dc0ef9e634
Tim Van Holder <tim.vanholder@gmail.com>
2020-04-14 23:53:25 +0200

    Add WriteList() and WriteListAsync() to JsonWriter.

--- MetaBrainz.Common.Json/Converters/JsonWriter.cs
+++ MetaBrainz.Common.Json/Converters/JsonWriter.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections.Generic;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.Threading.Tasks;
 
 using JetBrains.Annotations;
 
@@ -19,6 +21,37 @@ namespace MetaBrainz.Common.Json.Converters {
     /// <exception cref="NotSupportedException">Always.</exception>
     public sealed override T Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
       => throw new NotSupportedException("This converter is for serialization only.");
+
+    /// <summary>Writes a list of values as JSON.</summary>
+    /// <param name="writer">The writer to write to.</param>
+    /// <param name="values">The values to write.</param>
+    /// <param name="options">The options to use for serialization.</param>
+    public void WriteList(Utf8JsonWriter writer, IEnumerable<T> values, JsonSerializerOptions options) {
+      if (values == null) {
+        writer.WriteNullValue();
+        return;
+      }
+      writer.WriteStartArray();
+      foreach (var value in values)
+        this.Write(writer, value, options);
+      writer.WriteEndArray();
+    }
+
+    /// <summary>Writes a list of values as JSON.</summary>
+    /// <param name="writer">The writer to write to.</param>
+    /// <param name="values">The values to write.</param>
+    /// <param name="options">The options to use for serialization.</param>
+    /// <returns>A task that performs the writes.</returns>
+    public async Task WriteListAsync(Utf8JsonWriter writer, IAsyncEnumerable<T> values, JsonSerializerOptions options) {
+      if (values == null) {
+        writer.WriteNullValue();
+        return;
+      }
+      writer.WriteStartArray();
+      await foreach (var value in values)
+        this.Write(writer, value, options);
+      writer.WriteEndArray();
+    }
 
   }
 
