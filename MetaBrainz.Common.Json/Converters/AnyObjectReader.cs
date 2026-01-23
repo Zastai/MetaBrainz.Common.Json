0819340a6768cca533c11ee9306a1387dbd5016d
Tim Van Holder <tim.vanholder@gmail.com>
2026-01-23 23:51:57 +0100

    Tweak doc comments
    This also refactors the big `AnyObjectReader.Read()` method into a
    simple switch expression using help methods for the main contexts.

--- MetaBrainz.Common.Json/Converters/AnyObjectReader.cs
+++ MetaBrainz.Common.Json/Converters/AnyObjectReader.cs
@@ -24,12 +24,12 @@ public sealed class AnyObjectReader : JsonReader<object> {
   /// Specifies whether <see langword="null"/> should be allowed as a property value anywhere in the returned object.
   /// </param>
   /// <returns>
-  ///   The object converted from JSON. The specific type depends on the JSON token; see the table below.
-  ///   <list type="table">
-  ///     <listheader>
-  ///       <term>JSON Token</term>
-  ///       <description>Mapped Object Type</description>
-  ///     </listheader>
+  /// The object converted from JSON. The specific type depends on the JSON token; see the table below.
+  /// <list type="table">
+  ///   <listheader>
+  ///     <term>JSON Token</term>
+  ///     <description>Mapped Object Type</description>
+  ///   </listheader>
   ///   <item>
   ///     <term><c>null</c></term>
   ///     <description>
@@ -64,7 +64,9 @@ public sealed class AnyObjectReader : JsonReader<object> {
   ///       <para>If an integral value that fits in a 64-bit signed integer: <see cref="long"/>.</para>
   ///       <para>If an integral value that fits in a 64-bit unsigned integer: <see cref="ulong"/>.</para>
   ///       <para>If a value that fits in a .NET decimal: <see cref="decimal"/>.</para>
-  ///       <para>Otherwise, if it can be represented as a 64-bit double-precision floating point value: <see cref="double"/>.</para>
+  ///       <para>
+  ///         Otherwise, if it can be represented as a 64-bit double-precision floating point value: <see cref="double"/>.
+  ///       </para>
   ///       <para>Otherwise: <see cref="string"/>.</para>
   ///     </description>
   ///   </item>
@@ -86,168 +88,39 @@ public sealed class AnyObjectReader : JsonReader<object> {
   ///       <see cref="Dictionary{TKey,TValue}"/>, with <see cref="string"/> as key and (nullable) <see cref="object"/> as value.
   ///     </description>
   ///   </item>
-  ///   </list>
+  /// </list>
   /// </returns>
   /// <remarks>
-  ///   Unfortunately, <see cref="Utf8JsonReader.TryGetDecimal"/> will happily truncate decimals, even when a double would have
-  ///   preserved more digits. The degenerate case (where the decimal would be 0) is detected, but in other cases the less precise
-  ///   decimal will be used.
+  /// Unfortunately, <see cref="Utf8JsonReader.TryGetDecimal"/> will happily truncate decimals, even when a <c>double</c> would have
+  /// preserved more digits. The degenerate case (where the <c>decimal</c> would be 0) is detected, but in other cases the less
+  /// precise <c>decimal</c> will be used.
   /// </remarks>
   public static object Read(ref Utf8JsonReader reader, JsonSerializerOptions options, bool allowNullInArrays = true,
-                            bool allowNullAsPropertyValue = true) {
-    switch (reader.TokenType) {
-      // Easy cases
-      case JsonTokenType.False:
-        return false;
-      case JsonTokenType.True:
-        return true;
-      // Cases requiring further deduction
-      case JsonTokenType.Number: {
-        if (reader.TryGetInt32(out var i32)) {
-          return i32;
-        }
-        if (reader.TryGetInt64(out var i64)) {
-          return i64;
-        }
-        if (reader.TryGetUInt64(out var ui64)) {
-          return ui64;
-        }
-        {
-          decimal? dec;
-          double? fp;
-          if (reader.TryGetDecimal(out var decVal)) {
-            dec = decVal;
-          }
-          else {
-            dec = null;
-          }
-          if (reader.TryGetDouble(out var floatVal) && double.IsFinite(floatVal)) {
-            fp = floatVal;
-          }
-          else {
-            fp = null;
-          }
-          if (!dec.HasValue && fp.HasValue) {
-            // only double worked -> use it
-            return fp;
-          }
-          if (dec.HasValue && fp.HasValue) {
-            // check for a degenerate case: 1E-29 converts successfully to 0.0000000000000000000000000000m
-            // FIXME: 12E-29 converts to 0.0000000000000000000000000001m and 1.2E-28; ideally, we'd pick the double then too
-            if (dec.Value == 0 && Math.Abs(fp.Value) > double.Epsilon) {
-              return fp;
-            }
-            // otherwise, assume the decimal will be better
-            return dec;
-          }
-          // else: if converted to decimal but not double: should be impossible, so probably bad conversion: use fallback
-          // else: if neither decimal nor double: use fallback
-        }
-        return reader.GetRawStringValue();
-      }
-      case JsonTokenType.String: {
-        // Note: NOT TryGetBytesFromBase64() because that has many false positives (e.g. most ISRC values).
-        if (reader.TryGetDateTimeOffset(out var dto)) {
-          return dto;
-        }
-        if (reader.TryGetGuid(out var guid)) {
-          return guid;
-        }
-        var text = reader.GetStringValue();
-        if (Uri.TryCreate(text, UriKind.Absolute, out var uri)) {
-          return uri;
-        }
-        // FIXME: Are the other "special" strings we should recognize?
-        return text;
-      }
-      case JsonTokenType.StartArray: {
-        reader.Read();
-        if (reader.TokenType == JsonTokenType.EndArray) {
-          return Array.Empty<object?>();
-        }
-        var elements = new List<object?>();
-        while (reader.TokenType != JsonTokenType.EndArray) {
-          var element = reader.TokenType switch {
-            JsonTokenType.Null when allowNullInArrays => null,
-            _ => AnyObjectReader.Read(ref reader, options, allowNullInArrays, allowNullAsPropertyValue)
-          };
-          elements.Add(element);
-          reader.Read();
-        }
-        try {
-          // if all elements are the same type (or null), try to map it to an array of that type
-          Type? elementType = null;
-          var hasNulls = false;
-          foreach (var element in elements) {
-            if (element is null) {
-              hasNulls = true;
-              continue;
-            }
-            var t = element.GetType();
-            // ignore nullability
-            t = Nullable.GetUnderlyingType(t) ?? t;
-            if (elementType is null) {
-              elementType = t;
-            }
-            else if (elementType != t) {
-              elementType = null;
-              break;
-            }
-          }
-          if (elementType is not null) {
-            if (elementType.IsValueType && hasNulls) {
-              // make it nullable
-              elementType = typeof(Nullable<>).MakeGenericType(elementType);
-            }
-            var len = elements.Count;
-            var typedArray = Array.CreateInstance(elementType, len);
-            for (var i = 0; i < len; ++i) {
-              typedArray.SetValue(Convert.ChangeType(elements[i], elementType), i);
-            }
-            return typedArray;
-          }
-        }
-        catch {
-          // No worries, we still have the basic object array to return
-        }
-        return elements.ToArray();
-      }
-      case JsonTokenType.StartObject: {
-        reader.Read();
-        var obj = new Dictionary<string, object?>();
-        while (reader.TokenType != JsonTokenType.EndObject) {
-          if (reader.TokenType != JsonTokenType.PropertyName) {
-            throw new JsonException($"Expected a JSON object property, but received a {reader.TokenType} token instead.");
-          }
-          var prop = reader.GetPropertyName();
-          if (obj.ContainsKey(prop)) {
-            throw new JsonException($"Encountered a duplicate JSON object property ('{prop}').");
-          }
-          reader.Read();
-          obj[prop] = reader.TokenType switch {
-            JsonTokenType.Null when allowNullAsPropertyValue => null,
-            _ => AnyObjectReader.Read(ref reader, options, allowNullInArrays, allowNullAsPropertyValue)
-          };
-          reader.Read();
-        }
-        return obj;
-      }
-      default:
-        throw new JsonException($"Token ({reader.TokenType}: {reader.GetRawStringValue()}) cannot be converted to an object.");
-    }
-  }
+                            bool allowNullAsPropertyValue = true) => reader.TokenType switch {
+    // Easy cases
+    JsonTokenType.False => false,
+    JsonTokenType.True => true,
+    // Cases requiring further deduction
+    JsonTokenType.Number => AnyObjectReader.ReadValueFromNumber(ref reader),
+    JsonTokenType.String => AnyObjectReader.ReadValueFromString(ref reader),
+    JsonTokenType.StartArray
+      => AnyObjectReader.ReadArrayOfValues(ref reader, options, allowNullInArrays, allowNullAsPropertyValue),
+    JsonTokenType.StartObject
+      => AnyObjectReader.ReadObjectAsDictionary(ref reader, options, allowNullInArrays, allowNullAsPropertyValue),
+    _ => throw new JsonException($"Token ({reader.TokenType}: {reader.GetRawStringValue()}) cannot be converted to an object.")
+  };
 
   /// <summary>Reads and converts JSON to the most appropriate .NET framework type.</summary>
   /// <param name="reader">The reader to read from.</param>
   /// <param name="typeToConvert">The type of object to convert (ignored; assumed to be <see cref="object"/>).</param>
   /// <param name="options">The options to use for deserialization.</param>
   /// <returns>
-  ///   The object converted from JSON. The specific type depends on the JSON token; see the table below.
-  ///   <list type="table">
-  ///     <listheader>
-  ///       <term>JSON Token</term>
-  ///       <description>Mapped Object Type</description>
-  ///     </listheader>
+  /// The object converted from JSON. The specific type depends on the JSON token; see the table below.
+  /// <list type="table">
+  ///   <listheader>
+  ///     <term>JSON Token</term>
+  ///     <description>Mapped Object Type</description>
+  ///   </listheader>
   ///   <item>
   ///     <term><c>null</c></term>
   ///     <description>
@@ -281,7 +154,9 @@ public sealed class AnyObjectReader : JsonReader<object> {
   ///       <para>If an integral value that fits in a 64-bit signed integer: <see cref="long"/>.</para>
   ///       <para>If an integral value that fits in a 64-bit unsigned integer: <see cref="ulong"/>.</para>
   ///       <para>If a value that fits in a .NET decimal: <see cref="decimal"/>.</para>
-  ///       <para>Otherwise, if it can be represented as a 64-bit double-precision floating point value: <see cref="double"/>.</para>
+  ///       <para>
+  ///         Otherwise, if it can be represented as a 64-bit double-precision floating point value: <see cref="double"/>.
+  ///       </para>
   ///       <para>Otherwise: <see cref="string"/>.</para>
   ///     </description>
   ///   </item>
@@ -303,14 +178,150 @@ public sealed class AnyObjectReader : JsonReader<object> {
   ///       <see cref="Dictionary{TKey,TValue}"/>, with <see cref="string"/> as key and (nullable) <see cref="object"/> as value.
   ///     </description>
   ///   </item>
-  ///   </list>
+  /// </list>
   /// </returns>
   /// <remarks>
-  ///   Unfortunately, <see cref="Utf8JsonReader.TryGetDecimal"/> will happily truncate decimals, even when a double would have
-  ///   preserved more digits. The degenerate case (where the decimal would be 0) is detected, but in other cases the less precise
-  ///   decimal will be used.
+  /// Unfortunately, <see cref="Utf8JsonReader.TryGetDecimal"/> will happily truncate decimals, even when a <c>double</c> would have
+  /// preserved more digits. The degenerate case (where the <c>decimal</c> would be 0) is detected, but in other cases the less
+  /// precise <c>decimal</c> will be used.
   /// </remarks>
   public override object Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     => AnyObjectReader.Read(ref reader, options);
+
+  private static object ReadArrayOfValues(ref Utf8JsonReader reader, JsonSerializerOptions options, bool allowNullInArrays,
+                                          bool allowNullAsPropertyValue) {
+    reader.Read();
+    if (reader.TokenType == JsonTokenType.EndArray) {
+      return Array.Empty<object?>();
+    }
+    var elements = new List<object?>();
+    while (reader.TokenType != JsonTokenType.EndArray) {
+      var element = reader.TokenType switch {
+        JsonTokenType.Null when allowNullInArrays => null,
+        _ => AnyObjectReader.Read(ref reader, options, allowNullInArrays, allowNullAsPropertyValue)
+      };
+      elements.Add(element);
+      reader.Read();
+    }
+    try {
+      // if all elements are the same type (or null), try to map it to an array of that type
+      Type? elementType = null;
+      var hasNulls = false;
+      foreach (var element in elements) {
+        if (element is null) {
+          hasNulls = true;
+          continue;
+        }
+        var t = element.GetType();
+        // ignore nullability
+        t = Nullable.GetUnderlyingType(t) ?? t;
+        if (elementType is null) {
+          elementType = t;
+        }
+        else if (elementType != t) {
+          elementType = null;
+          break;
+        }
+      }
+      if (elementType is not null) {
+        if (elementType.IsValueType && hasNulls) {
+          // make it nullable
+          elementType = typeof(Nullable<>).MakeGenericType(elementType);
+        }
+        var len = elements.Count;
+        var typedArray = Array.CreateInstance(elementType, len);
+        for (var i = 0; i < len; ++i) {
+          typedArray.SetValue(Convert.ChangeType(elements[i], elementType), i);
+        }
+        return typedArray;
+      }
+    }
+    catch {
+      // No worries, we still have the basic object array to return
+    }
+    return elements.ToArray();
+  }
+
+  private static Dictionary<string, object?> ReadObjectAsDictionary(ref Utf8JsonReader reader, JsonSerializerOptions options,
+                                                                    bool allowNullInArrays, bool allowNullAsPropertyValue) {
+    reader.Read();
+    var obj = new Dictionary<string, object?>();
+    while (reader.TokenType != JsonTokenType.EndObject) {
+      if (reader.TokenType != JsonTokenType.PropertyName) {
+        throw new JsonException($"Expected a JSON object property, but received a {reader.TokenType} token instead.");
+      }
+      var prop = reader.GetPropertyName();
+      if (obj.ContainsKey(prop)) {
+        throw new JsonException($"Encountered a duplicate JSON object property ('{prop}').");
+      }
+      reader.Read();
+      obj[prop] = reader.TokenType switch {
+        JsonTokenType.Null when allowNullAsPropertyValue => null,
+        _ => AnyObjectReader.Read(ref reader, options, allowNullInArrays, allowNullAsPropertyValue)
+      };
+      reader.Read();
+    }
+    return obj;
+  }
+
+  private static object ReadValueFromNumber(ref Utf8JsonReader reader) {
+    if (reader.TryGetInt32(out var i32)) {
+      return i32;
+    }
+    if (reader.TryGetInt64(out var i64)) {
+      return i64;
+    }
+    if (reader.TryGetUInt64(out var u64)) {
+      return u64;
+    }
+    {
+      decimal? dec;
+      double? fp;
+      if (reader.TryGetDecimal(out var decVal)) {
+        dec = decVal;
+      }
+      else {
+        dec = null;
+      }
+      if (reader.TryGetDouble(out var floatVal) && double.IsFinite(floatVal)) {
+        fp = floatVal;
+      }
+      else {
+        fp = null;
+      }
+      if (!dec.HasValue && fp.HasValue) {
+        // only double worked -> use it
+        return fp;
+      }
+      if (dec.HasValue && fp.HasValue) {
+        // check for a degenerate case: 1E-29 converts successfully to 0.0000000000000000000000000000m
+        // FIXME: 12E-29 converts to 0.0000000000000000000000000001m and 1.2E-28; ideally, we'd pick the double then too
+        if (dec.Value == 0 && Math.Abs(fp.Value) > double.Epsilon) {
+          return fp;
+        }
+        // otherwise, assume the decimal will be better
+        return dec;
+      }
+      // else: if converted to decimal but not double: should be impossible, so probably bad conversion: use fallback
+      // else: if neither decimal nor double: use fallback
+    }
+    return reader.GetRawStringValue();
+  }
+
+  private static object ReadValueFromString(ref Utf8JsonReader reader) {
+    // Note: NOT TryGetBytesFromBase64() because that has many false positives (e.g. most ISRC values).
+    if (reader.TryGetDateTimeOffset(out var dto)) {
+      return dto;
+    }
+    if (reader.TryGetGuid(out var guid)) {
+      return guid;
+    }
+    var text = reader.GetStringValue();
+    if (Uri.TryCreate(text, UriKind.Absolute, out var uri)) {
+      return uri;
+    }
+    // FIXME: Are the other "special" strings we should recognize?
+    return text;
+  }
 
 }
